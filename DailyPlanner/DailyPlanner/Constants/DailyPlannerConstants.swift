022c5c2dd6229c9903ebd2e7fb9c24eeee4433db
Elena Vorontsova <lena.voroncova26@yandex.ru>
2023-04-18 16:57:25 +0400

    add: add monthLabel and monthButtons

---
+++ DailyPlanner/DailyPlanner/Constants/DailyPlannerConstants.swift
@@ -0,0 +1,17 @@
+//
+//  DailyPlannerConstants.swift
+//  DailyPlanner
+//
+//  Created by Lena Vorontsova on 18.04.2023.
+//
+
+import Foundation
+
+enum DailyPlannerConstants {
+    static let monthTop = 10
+    
+    static let monthButtonInset = 30
+    static let monthButtonOffset = 20
+    static let monthButtonHeight = 20
+    static let monthButtonWeight = 30
+}
---
+++ DailyPlanner/DailyPlanner/Modules/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  DailyPlanner
+//
+//  Created by Lena Vorontsova on 17.04.2023.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+    private var monthLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Month"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.textAlignment = .center
+        return label
+    }()
+    private var monthLeftButton: UIButton = {
+        let button = UIButton()
+        if #available(iOS 13.0, *) {
+            button.setImage(UIImage(systemName: "arrowshape.left.fill"), for: .normal)
+        } else {
+            button.setTitle("Next", for: .normal)
+        }
+        return button
+    }()
+    private var monthRightButton: UIButton = {
+        let button = UIButton()
+        if #available(iOS 13.0, *) {
+            button.setImage(UIImage(systemName: "arrowshape.right.fill"), for: .normal)
+        } else {
+            button.setTitle("Previous", for: .normal)
+        }
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.navigationItem.title = "Daily Planner"
+        self.configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        self.view.addSubview(monthLabel)
+        self.view.addSubview(monthLeftButton)
+        self.view.addSubview(monthRightButton)
+        monthLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(DailyPlannerConstants.monthTop)
+            $0.leading.equalTo(monthLeftButton.snp.trailing).offset(DailyPlannerConstants.monthButtonOffset)
+            $0.trailing.equalTo(monthRightButton.snp.leading).offset(-DailyPlannerConstants.monthButtonOffset)
+        }
+        monthLeftButton.snp.makeConstraints {
+            $0.height.width.equalTo(DailyPlannerConstants.monthButtonHeight)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(DailyPlannerConstants.monthTop)
+            $0.leading.equalToSuperview().inset(DailyPlannerConstants.monthButtonInset)
+//            $0.trailing.equalTo(monthLabel.snp.leading).offset(-DailyPlannerConstants.monthButtonOffset)
+        }
+        monthRightButton.snp.makeConstraints {
+            $0.height.width.equalTo(DailyPlannerConstants.monthButtonHeight)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(DailyPlannerConstants.monthTop)
+//            $0.leading.equalTo(monthLabel.snp.leading).offset(DailyPlannerConstants.monthButtonOffset)
+            $0.trailing.equalToSuperview().inset(DailyPlannerConstants.monthButtonInset)
+        }
+    }
+}
+
